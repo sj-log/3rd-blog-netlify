fe870d8a721cdbfb0b83fd695902bb2437cde9a2
SJ <sjisyours@gmail.com>
2019-11-28 23:22:45 +0900

    scss update and font

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../scss/styles.scss'
-import {Link} from 'react-router-dom';
 
 var Intro = () => {
+    var imgs = ['https://user-images.githubusercontent.com/35059428/69769074-9806ca00-11c6-11ea-979e-cc45f1c5f2cb.png','https://user-images.githubusercontent.com/35059428/69770220-9ee40b80-11cb-11ea-9f4f-5d0c38a95929.png','https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104' ]
+        
+        
+    // var i= -1;
+    // setInterval(() => {
+        
+    //     i= i+1;
+    
+    //     if(i==2){
+    //         i=-1;
+    //     }
+
+    // }, 5000);
+  
+
+
    return (
         <section className="body-section">
-            <section className="img-section">
-             <img alt="Sj Vipassana Study" src="https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104"></img>
-                {/* <img src="https://scontent-sin2-1.cdninstagram.com/vp/1c14a1efa3a8fae5465243b00ada8a88/5E23DEC0/t51.2885-15/e35/47581801_323181528283940_4634403341321352258_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=108"></img> */}
-                {/* <img
-                    src="https://scontent-sin6-1.cdninstagram.com/vp/b32f778adfe5fa3822f4a0e54edafef8/5E23D78C/t51.2885-15/e35/s1080x1080/67673091_890824034610624_4093358327862933201_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=100"></img> */}
-            </section>
+            <section className="img-section carousel" data-interval="6000">
+             <img id='carousel-img' alt="Sj Vipassana Study" src='https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104'></img>
+           </section>
             {/* <aside className="text-section">
                 <h1>Good things with freedom</h1>
                 <p>Have been traveled world, to make better version of myself.</p>
