9ac733f820bc8ec533ce5047805679a2131298ea
SJ <sjisyours@gmail.com>
2019-12-07 14:11:01 +0900

    up

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -3,51 +3,54 @@ import {Helmet} from "react-helmet";
 
 export default class Application extends React.Component {
     render() {
-        return (<div className="application">
-            <Helmet defaultTitle="log" titleTemplate="%s | Sj">
+        return (
+            <div className="application">
+                <Helmet defaultTitle="log" titleTemplate="%s | Sj">
 
-                <meta charSet="utf-8"/>
-                <meta name="description" content="Sj Blog Article"/>
-                <meta property="og:type" content="article"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="theme-color" content="#000000"/>
+                    <meta charSet="utf-8"/>
+                    <meta name="description" content="Sj Blog Article"/>
+                    <meta property="og:type" content="article"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <meta name="theme-color" content="#000000"/>
 
-                <title itemProp="name" lang="en">{`Log`}</title>
+                    <title itemProp="name" lang="en">{`Log`}</title>
 
-                <link rel="shortcut icon" href="/PUBLIC_URL//favicon.ico"/>
-                <link rel="manifest" href="/PUBLIC_URL/manifest.json"/>
-                <link rel="apple-touch-icon" href="logo192.png"/>
-                <link rel="canonical" href="http://mysite.com/example"/>
+                    <link rel="shortcut icon" href="/PUBLIC_URL//favicon.ico"/>
+                    <link rel="manifest" href="/PUBLIC_URL/manifest.json"/>
+                    <link rel="apple-touch-icon" href="logo192.png"/>
+                    <link rel="canonical" href="http://mysite.com/example"/>
 
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                
-                
-                {/* Google Search Console */}
-                <meta name="google-site-verification" content="WEsHUyxBDviVZl97dxUfxTM97JitX-6kDwmSPRE7jMA" />
-              
-                {/* Google Adsense */}
-                <script>
-                    {` (adsbygoogle = window.adsbygoogle || []).push({
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+                    {/* Google Search Console */}
+                    <meta
+                        name="google-site-verification"
+                        content="WEsHUyxBDviVZl97dxUfxTM97JitX-6kDwmSPRE7jMA"/> {/* Google Adsense */}
+                    <script>
+                        {` (adsbygoogle = window.adsbygoogle || []).push({
                         google_ad_client: "ca-pub-9404465753932962",
                         enable_page_level_ads: true
-                    });`}
-                </script>
+                    });`}</script>
 
-              {/* Google Analytics */}
-                <script
-                    async="async"
-                    src="https://www.googletagmanager.com/gtag/js?id=UA-116676814-1"></script>
-                <script>
-                    {
-                        `  window.dataLayer = window.dataLayer || [];
+                    {/* Google Analytics */}
+                    <script
+                        async="async"
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-116676814-1"></script>
+                    <script>
+                        {`  window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
 
                             gtag('config', 'UA-116676814-1');`
-                    }</script>
+}</script>
+{/* typed js */}
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
 
-            </Helmet>
-        </div>
+
+                </Helmet>
+            </div>
         );
     }
 };
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import '../scss/styles.scss'
+import '../scss/styles.scss';
+import Typed from 'react-typed';
 
 var Intro = () => {
-    var imgs = ['https://user-images.githubusercontent.com/35059428/69769074-9806ca00-11c6-11ea-979e-cc45f1c5f2cb.png','https://user-images.githubusercontent.com/35059428/69770220-9ee40b80-11cb-11ea-9f4f-5d0c38a95929.png','https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104' ]
-        
+    var imgs = [
+        'https://user-images.githubusercontent.com/35059428/69769074-9806ca00-11c6-11ea-9' +
+                '79e-cc45f1c5f2cb.png',
+        'https://user-images.githubusercontent.com/35059428/69770220-9ee40b80-11cb-11ea-9' +
+                'f4f-5d0c38a95929.png',
+        'https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E2' +
+                '78B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=' +
+                'scontent-sin2-1.cdninstagram.com&_nc_cat=104'
+    ]
 
-   return (
+    return (
         <section className="body-section">
-            <section className="img-section carousel" data-interval="6000">
-             <img id='carousel-img' alt="Sj Vipassana Study" src='https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104'></img>
-           </section>
-        
+            <section className="img-section carousel">
+                <img
+                    id='carousel-img'
+                    alt="Sj Vipassana Study"
+                    src='https://scontent-sin2-1.cdninstagram.com/vp/dce8f3d6e157f4081f51751074023a11/5E278B55/t51.2885-15/e35/45350830_808270686177244_7067872797703318700_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=104'></img>
+            </section>
+            <aside className="text-section">
+            <Typed
+                strings={[
+                    "Hello! I'm SJ",
+                    "I'm searching for a way to make this world better",
+                    "Through Writing code and text."]}
+                    typeSpeed={70}
+                    backSpeed={50}
+                    attr=""
+                    loop >
+                    <span type="text"/>
+                </Typed>
+            </aside>
         </section>
     )
 
