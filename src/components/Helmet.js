9a4a485cf83730d825f9249bba39da35a0725256
SJ <sjisyours@gmail.com>
2019-12-14 13:17:26 +0900

    added '/js' in robots.txt

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -5,17 +5,20 @@ export default class Application extends React.Component {
     render() {
         return (
             <div className="application">
-                <Helmet defaultTitle="log" titleTemplate="%s | Sj" >
+                <Helmet defaultTitle="log" titleTemplate="%s | Sj">
 
                     <meta charSet="utf-8"/>
-                    <meta name="description" content="Sj Blog Article"/>
+                    
+                    <meta name="description" content="A little world of mine"/>
                     <meta property="og:type" content="article"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                    <meta name="theme-color" content="#000000"/>
-{/* search console verification tag */}
-<meta name="google-site-verification" content="WEsHUyxBDviVZl97dxUfxTM97JitX-6kDwmSPRE7jMA" />
-<meta name="naver-site-verification" content="3fe947b693663b49943a1272b8cb8ae6d2e5082f"/>
-
+                    <meta name="theme-color" content="#000000"/> {/* search console verification tag */}
+                    <meta
+                        name="google-site-verification"
+                        content="WEsHUyxBDviVZl97dxUfxTM97JitX-6kDwmSPRE7jMA"/>
+                    <meta
+                        name="naver-site-verification"
+                        content="3fe947b693663b49943a1272b8cb8ae6d2e5082f"/>
 
                     <title itemProp="name" lang="en">{`Log`}</title>
 
@@ -27,23 +30,22 @@ export default class Application extends React.Component {
                         async
                         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
-{/* <!-- Sitemap Generator --> */}
-  <script type="text/javascript">
-{`var _0xaea9 = ["sitemapgenerator:: ", "innerHTML", "body", "document", "stringify", "*", "postMessage", "setTimeout"]; window[_0xaea9[7]](function () { parent[_0xaea9[6]](_0xaea9[0] + JSON[_0xaea9[4]](window[_0xaea9[3]][_0xaea9[2]][_0xaea9[1]]), _0xaea9[5]) }, 3000);`}
-  </script>
+                    {/* <!-- Sitemap Generator --> */}
+                    <script type="text/javascript">
+                        {`var _0xaea9 = ["sitemapgenerator:: ", "innerHTML", "body", "document", "stringify", "*", "postMessage", "setTimeout"]; window[_0xaea9[7]](function () { parent[_0xaea9[6]](_0xaea9[0] + JSON[_0xaea9[4]](window[_0xaea9[3]][_0xaea9[2]][_0xaea9[1]]), _0xaea9[5]) }, 3000);`}</script>
 
-{/* Google adsense installed */}
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>{`
+                    {/* Google adsense installed */}
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <script>{`
      (adsbygoogle = window.adsbygoogle || []).push({
           google_ad_client: "ca-pub-9404465753932962",
           enable_page_level_ads: true
-     });`}
-</script>
-                  
-{/* typed js */}
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+     });`}</script>
 
+                    {/* typed js */}
+                    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
 
                 </Helmet>
             </div>
